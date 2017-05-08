18b8b27ba456edd5032e7e310d551acd4ea85e78
Gordon Bateson <gordonbateson@gmail.com>
2017-05-09 08:35:14 +0900

    block_maj_submissions add dividers to conference tools section of block on course page

--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2017042068;
-$plugin->release   = '2017-04-20 (68)';
+$plugin->version   = 2017050869;
+$plugin->release   = '2017-05-08 (69)';
