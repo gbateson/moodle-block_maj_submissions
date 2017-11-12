1bc0e2e16cacef65896af4eae84efd5685dd69ad
Gordon Bateson <gordonbateson@gmail.com>
2017-11-12 21:29:46 +0900

    block/maj_submissions reduce multiline list of equipment items, to single-line comma-separated string

--- presets/rooms.js
+++ presets/rooms.js
@@ -6,5 +6,18 @@ MAJ.onload_edit = function() {
 MAJ.onload_view = function() {
     MAJ.reduce_lang_strings(".defaulttemplate th:not(.actions)," +
                             ".defaulttemplate tr:not(.photo) td");
+    MAJ.reduce_multiline_list(".defaulttemplate .equipment td");
 }
 
+MAJ.reduce_multiline_list = function(elements) {
+    var br1 = new RegExp("^(\\s|<br[^>]*>)+");
+    var br2 = new RegExp("(\\s|<br[^>]*>)+$");
+    var br3 = new RegExp("\\s*<br[^>]*>\\s*", "g");
+    var html = $(elements).each(function(){
+        var html = $(this).html();
+        html = html.replace(br1, "");
+        html = html.replace(br2, "");
+        html = html.replace(br3, ", ");
+        $(this).html(html);
+    });
+}
--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2017111222;
-$plugin->release   = '2017-11-12 (22)';
+$plugin->version   = 2017111223;
+$plugin->release   = '2017-11-12 (23)';
