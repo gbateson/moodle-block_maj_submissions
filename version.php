c2f2198c99e4a0565d7bcb05e311c1e76a984f59
Gordon Bateson <gordonbateson@gmail.com>
2017-12-20 18:09:27 +0900

    block/maj_submissions move position of early bird setting on block settings form

--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2017122045;
-$plugin->release   = '2017-12-20 (45)';
+$plugin->version   = 2017122046;
+$plugin->release   = '2017-12-20 (46)';
