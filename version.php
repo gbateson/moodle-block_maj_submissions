c16a61040bc04206dd2107d48b36d26fe991b1e5
Gordon Bateson <gordonbateson@gmail.com>
2018-01-10 13:48:21 +0900

    block/maj_submissions bump version number to trigger upgrade to previous release

--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2018010852;
-$plugin->release   = '2018-01-08 (52)';
+$plugin->version   = 2018011053;
+$plugin->release   = '2018-01-10 (53)';
