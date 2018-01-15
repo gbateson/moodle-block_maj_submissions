5dbfca6f9190bd7426539e83d9fdd8e078222281
Gordon Bateson <gordonbateson@gmail.com>
2018-01-15 17:57:31 +0900

    block/maj_submissions lighten backgroun and border of Attending elements on schedule

--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2018011563;
-$plugin->release   = '2018-01-15 (63)';
+$plugin->version   = 2018011564;
+$plugin->release   = '2018-01-15 (64)';
