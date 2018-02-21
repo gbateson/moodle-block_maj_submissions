7445369b94ac0f96f25797f5456a95156eff6b83
Gordon Bateson <gordonbateson@gmail.com>
2018-02-21 10:47:24 +0900

    block/maj_submissions improve printing of registration docuements with small adjustmnents to CSS

--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2018022076;
-$plugin->release   = '2018-02-20 (76)';
+$plugin->version   = 2018022077;
+$plugin->release   = '2018-02-20 (77)';
