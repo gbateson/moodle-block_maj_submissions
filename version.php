4f8390b9557dd2c7a09bfb19d0a0c62eb3e75ad8
Gordon Bateson <gordonbateson@gmail.com>
2018-07-11 13:51:17 +0900

    blocks/maj_submissions regenerate AMD files

--- version.php
+++ version.php
@@ -35,5 +35,5 @@ $plugin->dependencies = array(
 );
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2018071198;
-$plugin->release   = '2018-07-11 (98)';
+$plugin->version   = 2018071199;
+$plugin->release   = '2018-07-11 (99)';
